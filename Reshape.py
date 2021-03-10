25ad838b10cd823e67a3823ac8bd95269e5231b1
JonathonHowland <howlandjonathon@gmail.com>
2021-03-09 20:03:34 -0600

    File for reshaping the dataset

---
+++ Reshape.py
@@ -0,0 +1,13 @@
+#File for reshaping the dataset
+
+import pandas as pd
+import numpy as np
+
+
+#Import sheets from Excel as dataframes
+
+UNPop = pd.read_excel("BACCFromPython.xlsx", sheet_name="UN Population")
+IHMEPop = pd.read_excel("BACCFromPython.xlsx", sheet_name="IHME Population")
+WHO = pd.read_excel("BACCFromPython.xlsx", sheet_name="WHO Vaccine Coverage")
+IHMEVacc = pd.read_excel("BACCFromPython.xlsx", sheet_name="IHME Vaccine Coverage")
+Vacc = pd.read_excel("BACCFromPython.xlsx", sheet_name="Vaccine_Impact_Data")
