3e06ef0b9ea161def0115723cac1f903e39a71e1
JonathonHowland <howlandjonathon@gmail.com>
2021-03-15 22:26:05 -0500

    Code for optimization

--- PrescriptiveAnalysis.py
+++ PrescriptiveAnalysis.py
@@ -2,7 +2,9 @@
 
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
 from scipy.stats.distributions import chi2
+import matplotlib.pyplot as plt
 
 
 #Import sheets from Excel as dataframes
@@ -16,26 +18,54 @@ Vacc = pd.read_excel("BACCFromPython.xlsx", sheet_name="Vaccine_Impact_Data")
 #Rename age_group_name from IHME to age_group
 #IHMEPop = IHMEPop.rename(columns={"age_group_name": "age_group"})
 
-print(IHMEPop.head(100))
-
 PopTot = pd.merge(UNPop, IHMEPop, how="inner", left_on=["iso_code", "age_group", "year"], right_on=["iso_code", "age_group_name", "year"])
 
-print(PopTot)
+#Merge datasets
+VaccTot = pd.merge(WHO, IHMEVacc, how="inner", left_on=["iso_code", "year"], right_on=["iso_code", "year"])
 
-#Perform the chi^2 test
+xList = ["mcv2_coverage", "Hib3_coverage", "pcv3_coverage", "rota_coverage"]
 
-R = PopTot["population"].sum()
-S = PopTot["reference"].sum()
-N = len(PopTot)
-chisq = 0
+XList = ["MCV2", "Hib3", "PCV3", "Rota"]
 
-for i in range(N):
-    chisq += (np.sqrt(S/R)*PopTot["population"][i] - np.sqrt(R/S)*PopTot["reference"][i])**2/(PopTot["population"][i] + PopTot["reference"][i])
+#Look at the averages between the two datasets
+for i in range(len(xList)):
+    xString = xList[i] + "_x"
+    yString = xList[i] + "_y"
+    VaccTot[xList[i]] = 0.5*(VaccTot[xString] + VaccTot[yString])
+    #Drop the old columns
+    VaccTot = VaccTot.drop(columns=[xString, yString])
+                             
+print(VaccTot)
 
-chiScaled = chisq/N
+print(VaccTot.columns)
 
-print("Chi^2 = {}, scaled Chi^2 = {}".format(chisq, chiScaled))
+#Find the average by iso_code
+#VaccAvg = VaccTot.groupby(["iso_code"]).mean()
+#print(VaccAvg)
 
-pVal = chi2.sf(chisq, N-1)
 
-print("P-value = {}".format(pVal))
+#Merge the datasets
+Final = pd.merge(VaccTot, Vacc, how="inner", left_on=["iso_code"], right_on=["country"])
+
+print(Final)
+
+print(Final.columns)
+
+#Make a list of datasets (one for each vaccine)
+Mcv2 = Final[Final["vaccine"]=="MCV2"]
+Mcv2 = Mcv2.rename(columns={"mcv2_coverage": "coverage"})
+Hib3 = Final[Final["vaccine"]=="Hib3"]
+Hib2 = Hib3.rename(columns={"Hib3_coverage": "coverage"})
+Pcv3 = Final[Final["vaccine"]=="PCV3"]
+Pcv3 = Pcv3.rename(columns={"pcv3_coverage": "coverage"})
+Rota = Final[Final["vaccine"]=="Rota"]
+Rota = Rota.rename(columns={"rota_coverage": "coverage"})
+
+dfList = [Mcv2, Hib3, Pcv3, Rota]
+
+for df in dfList:
+    print(df)
+    print(df.columns)
+
+
+    
